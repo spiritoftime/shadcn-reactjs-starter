703ba0a621ac23ead5dafce36c56880ca8cb15af
spiritoftime <chooliangyi6@gmail.com>
2023-07-10 23:12:33 +0800

    commit starter code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+import { AlertDialogDemo } from "./components/AlertDialogDemo";
+
+function App() {
+  return (
+    <>
+      <p>Hola</p>
+      <AlertDialogDemo />
+    </>
+  );
+}
+
+export default App;
